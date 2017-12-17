046f0136a2e1e98dd0ec7d4b98dd2600a3648812
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2017-12-17 23:59:32 +0100

    method getDifferenceDays() returns difference beetwen dates in seconds with no need to convert to other time units

--- Engine.java
+++ Engine.java
@@ -6,6 +6,6 @@ import java.util.concurrent.TimeUnit;
 public class Engine {
     public static long getDifferenceDays(Date d1, Date d2) {
         long diff = d2.getTime() - d1.getTime();
-        return TimeUnit.DAYS.convert(diff, TimeUnit.SECONDS);
+        return diff;//TimeUnit.SECONDS.convert(diff, TimeUnit.SECONDS);
     }
 }
