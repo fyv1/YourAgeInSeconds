9563da5286ad7b7fd3448e0024c16f57794c79ae
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2017-12-17 15:22:21 +0100

    Engine class: Difference method added; Main: todo formating data objects

--- Engine.java
+++ Engine.java
@@ -1,5 +1,11 @@
 package pl.fyv;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Engine {
-    public int todo=0;
+    public static long getDifferenceDays(Date d1, Date d2) {
+        long diff = d2.getTime() - d1.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.SECONDS);
+    }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package pl.fyv;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -10,16 +11,25 @@ public class Main {
 
         Scanner buffer = new Scanner(System.in);
 
-       /* System.out.println("Twoj wiek w sekundach");
+        System.out.println("Twoj wiek w sekundach");
         System.out.println("Podaj dzien urodzenia: "); int d = buffer.nextInt();
         System.out.println("Podaj miesiac: "); int m = buffer.nextInt();
         System.out.println("Podaj rok: "); int y = buffer.nextInt();
 
-        User u = new User(d,m,y);*/
+        User u = new User(d,m,y);
 
+        //todo formating data objects
 
         Calendar today = Calendar.getInstance();
-        System.out.println(new SimpleDateFormat("MM-MM-yyyy").format(today.getTime()));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yy");
+        Date todayD = today.getTime();
+        sdf.format(todayD);
+        System.out.println(todayD);
 
+        Calendar bday = Calendar.getInstance();
+        bday.set(u.getYear(), u.getMonth()-1, u.getDay());
+        Date bdayD = bday.getTime();
+        sdf.format(bdayD);
+        System.out.println(bdayD);
     }
 }
