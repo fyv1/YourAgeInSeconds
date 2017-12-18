774ad1d7fa37a14bd931208a43506783d12705bb
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2017-12-18 12:58:48 +0100

    cleaning main method, whole mechanism is now in Engine class

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 package pl.fyv;
 
-import java.util.Calendar;
-import java.util.Date;
+
 import java.util.Scanner;
 
 public class Main {
@@ -13,13 +12,6 @@ public class Main {
 
         e.getData(buffer);
 
-        Calendar today = Calendar.getInstance();
-        Date todayD = today.getTime();
-        Calendar bday = Calendar.getInstance();
-        bday.set(u.getYear(), u.getMonth()-1, u.getDay());
-        Date bdayD = bday.getTime();
-
-
-        System.out.println("Twoj wiek w sekundach: "+ e.getDifferenceDays(bdayD, todayD));
+        System.out.println("Twoj wiek w sekundach: "+ e.getDifferenceDays(e.getBdayD(), e.getTodayD()));
     }
 }
